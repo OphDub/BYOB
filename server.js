0727612f5dc0210e1c1c39b419bbb05bf4859d06
Ophus Wong <ophus.wong@gmail.com>
2018-03-26 15:22:26 -0600

    Add server js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'BYOB';
+
+app.get('/', (request, response) => {
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
