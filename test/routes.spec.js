6fb1e20ff310536e54cfaab966f86478d658a1b5
Ophus Wong <ophus.wong@gmail.com>
2018-03-28 13:02:32 -0600

    Add test env config to routes spec test file

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('API Routes', () => {
